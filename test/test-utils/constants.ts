3a7679f40d81462cb1c7a9cdb686a7e2dd0f5084
hanseltime <hanseltime@gmail.com>
2023-09-30 13:05:25 -0600

    fix: update the temp file to be outside of lib

--- test/test-utils/constants.ts
+++ test/test-utils/constants.ts
@@ -1,3 +1,5 @@
 import path from 'path'
 
-export const TEMP_SYNTH_OUT_FOLDER = path.resolve(__dirname, 'temp.out')
+// Note - we make the temp dir outside of the source dir so that typescript rules
+// don't panic on the temp directory's typescript files
+export const TEMP_SYNTH_OUT_FOLDER = path.resolve(__dirname, '..', '..', 'temp.out')
